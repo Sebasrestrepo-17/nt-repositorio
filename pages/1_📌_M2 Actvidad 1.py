651de97c5aa6675aa864d8f6bfcbdf97f388a9ec
Sebas <sebas4831441@gmail.com>
2025-05-09 19:08:11 -0500

    ...

--- pages/1_📌_M2 Actvidad 1.py
+++ pages/1_📌_M2 Actvidad 1.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 # Configuración de la página
 st.set_page_config(   
@@ -27,3 +28,27 @@ st.markdown("""
 
 st.header("Solución")
 
+
+st.header("Actividad #1 -- Creacion de DataFrames")
+
+st.write("Objetivo de la actividad: Demostrar lo aprendido sobre la creacion de Dataframes con Pandas")
+
+
+st.subheader("Dataframe de Libros -- Diccionario")
+Libros = {
+    "Nombre": ["El caballero de la armadura oxidada", "El coronel no tiene quien le escriba", "El principito"],
+    "Autor" : ["Robert Fisher", "Gabriel García Marquez", "Antoine de Saint-Exupéry"],
+    "Año" : ["1991", "1961", "1943"],
+    "Genero" : ["Novela", "Novela", "Novela"]
+}
+
+df = pd.DataFrame(Libros)
+st.write(df)
+
+st.subheader("Dataframe de Informacion de ciudades -- Listas de Diccionarios")
+ciudades = [{"Nombre" : "Medellin", "Poblacion" : " 2 533 424", "Pais" : "Colombia"},
+            {"Nombre" : "Bogotá", "Poblacion" : "11 795 800", "Pais" : "Colombia"},
+            {"Nombre" : "Cali", "Poblacion" : "2 294 653 ", "Pais" : "Colombia"}]
+
+df = pd.DataFrame(ciudades)
+st.write(df)
